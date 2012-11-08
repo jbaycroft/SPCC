6c5f3e273fe1761ce91c52ce36f784ec58688564
John Baycroft <navmusic@gmail.com>
2012-11-08 11:46:30 -0500

    Application Shell Generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SPCC::Application.config.secret_token = 'c9d3926350228296719b0c4fb4db8a065bc02179ddf344b28cc8e688461bf1747263b0c114d863c394db386d942d9cd7bdb8a6477796c7975eafa9cd7f31ece5'
