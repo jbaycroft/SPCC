cefd093179b61e9e5d492ac2aaf9707f27529fba
John Baycroft <navmusic@gmail.com>
2012-11-13 11:54:16 -0500

    fixed inproper field reference

--- app/models/storage.rb
+++ app/models/storage.rb
@@ -1,4 +1,4 @@
 class Storage < ActiveRecord::Base
   belongs_to :client
-  attr_accessible :cap, :direction, :drainnear, :fillprocedures, :leakdetect, :leakdetect_yes, :loc, :material, :overfillprot, :overfillprot_yes, :potentialfail, :potentialrate, :potentialrelease, :throughput, :type
+  attr_accessible :cap, :direction, :drainnear, :fillprocedures, :leakdetect, :leakdetect_yes, :loc, :material, :overfillprot, :overfillprot_yes, :potentialfail, :potentialrate, :potentialrelease, :throughput, :tanktype
 end
